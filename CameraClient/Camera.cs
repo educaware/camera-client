03c728c2da929ff369f068c4a665a28e64899f11
rmenai <rami.menai@outlook.com>
2021-12-11 20:19:20 +0300

    Initial commit

---
+++ CameraClient/Camera.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+using Emgu.CV;
+using Websocket.Client;
+
+namespace CameraClient
+{
+    public class Camera
+    {
+        private VideoCapture? _capture;
+        private readonly WebsocketClient _client;
+
+        // private VideoQuality quality;
+        private readonly ImageFormat _imageFormat;
+
+        public Camera(WebsocketClient client)
+        {
+            _client = client;
+
+            // Set video encoding.
+            _imageFormat = ImageFormat.Jpeg;
+        }
+
+        private byte[] Compress(Mat? image)
+        {
+            var ms = new MemoryStream();
+            image.ToBitmap().Save(ms, _imageFormat);
+            return ms.ToArray();
+        }
+
+        private void ProcessFrame(object? sender, EventArgs arg)
+        {
+            var imageFrame = _capture?.QueryFrame();
+            byte[] bytes = Compress(imageFrame);
+            _client.Send(bytes);
+        }
+
+        public void TurnOn()
+        {
+            // Initialise `capture` if it is null.
+            _capture ??= new VideoCapture(0, VideoCapture.API.DShow);
+            _capture.ImageGrabbed += ProcessFrame;
+            _capture.Start();
+        }
+
+        public void TurnOff()
+        {
+            // Stop and dispose `capture` only if it is not null.
+            _capture?.Stop();
+            _capture?.Dispose();
+
+            // Set it to `null` to turn off the camera.
+            _capture = null;
+        }
+
+        public void Blink(int repeat, int delay)
+        {
+            for (var i = 0; i < repeat; i++)
+            {
+                TurnOn();
+                Thread.Sleep(delay);
+                
+                TurnOff();
+                Thread.Sleep(delay);
+            }
+        }
+    }
+}
---
+++ CameraClient/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Threading;
+using CameraClient;
+using Websocket.Client;
+
+var url = new Uri("ws://localhost:8080/api/v1/ws");
+var exitEvent = new ManualResetEvent(false);
+
+
+var client = new WebsocketClient(url);
+var camera = new Camera(client);
+
+// Change client settings.
+client.ReconnectTimeout = TimeSpan.FromMinutes(20);
+client.ErrorReconnectTimeout = TimeSpan.FromSeconds(30);
+
+// Subscribe to specific events.
+client.MessageReceived.Subscribe(ParseMessage);
+
+// Start and keep the connection alive.
+await client.Start();
+exitEvent.WaitOne();
+
+
+void ParseMessage(ResponseMessage msg)
+{
+    switch (msg.ToString())
+    {
+        case "on":
+            camera.TurnOn();
+            break;
+
+        case "off":
+            camera.TurnOff();
+            break;
+
+        case { } s when s.StartsWith("blink"):
+            // Parse the response.
+            var strings = s.Split(",");
+            var repeat = Convert.ToInt32(strings[1]);
+            var delay = Convert.ToInt32(strings[2]);
+
+            camera.Blink(repeat, delay);
+            break;
+
+        case "close":
+            // Exit the program safely.
+            Environment.Exit(0);
+            break;
+    }
+}
